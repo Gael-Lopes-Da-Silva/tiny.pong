92ffa1a5a272ee6f46fe9567d095704dfc203888
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2022-07-28 13:53:44 +0200

    REMOVED: changed the build script

--- Source/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set INPUT= main.c
-set OUTPUT= main.exe
-set FLAGS= -o%OUTPUT% -Wall -Wextra -pedantic -fno-common -fno-builtin
-set LIBS= -lraylib -lopengl32 -lgdi32 -lwinmm
-
-clang %INPUT% %FLAGS% %LIBS%
-
-if ERRORLEVEL == 0 %OUTPUT%
