70139ac4020a2f576c0d9708ebd2d88f0b449b37
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2023-08-01 13:07:45 +0200

    formated the files and updated the headers

--- src/pong.c
+++ src/pong.c
@@ -1,175 +1,157 @@
-/* 
-@author: Gael Lopes Da Silva
-@project: Pong
-@github: https://github.com/Gael-Lopes-Da-Silva/Pong
-@gitlab: https://gitlab.com/Gael-Lopes-Da-Silva/Pong
-*/
+// @author: Gael Lopes Da Silva
+// @project: Pong
+// @github: https://github.com/Gael-Lopes-Da-Silva/Pong
 
 #ifndef RAYLIB_LOADED
-    #define RAYLIB_LOADED
-    #include "../include/raylib.h"
-    #include "../include/raymath.h"
-    #include "../include/reasings.h"
+#define RAYLIB_LOADED
+#include "../include/raylib.h"
+#include "../include/raymath.h"
+#include "../include/reasings.h"
 #endif
 
 #include "pong.h"
 
-void player_Draw(Player player)
-{
-    DrawRectangleRec(player.texture, player.color);
+void player_Draw(Player player) {
+  DrawRectangleRec(player.texture, player.color);
 }
 
-void player_UpdatePosition(Player *player)
-{
-    if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) player->position -= player->speed;
-    if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) player->position += player->speed;
+void player_UpdatePosition(Player *player) {
+  if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP))
+    player->position -= player->speed;
+  if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN))
+    player->position += player->speed;
 
-    player->position = Clamp(player->position, 10, SCREEN_HEIGHT - 10 - (player->texture.height));
-    player->texture.y = player->position;
+  player->position = Clamp(player->position, 10, SCREEN_HEIGHT - 10 - (player->texture.height));
+  player->texture.y = player->position;
 }
 
-void enemy_Draw(Enemy enemy)
-{
-    DrawRectangleRec(enemy.texture, enemy.color);
-}
+void enemy_Draw(Enemy enemy) { DrawRectangleRec(enemy.texture, enemy.color); }
 
-void enemy_UpdatePosition(Enemy *enemy, Ball ball)
-{
-    if (ball.position.x > SCREEN_WIDTH / 2)
-    {
-        if (ball.position.y - ball.size < enemy->position)
-        {
-            enemy->position -= enemy->speed;
-        }
-        else if (ball.position.y + ball.size > (enemy->position) + (enemy->texture.height))
-        {
-            enemy->position += enemy->speed;
-        }
-
-        enemy->position = Clamp(enemy->position, 10, SCREEN_HEIGHT - 10 - (enemy->texture.height));
-        enemy->texture.y = enemy->position;
-    }
-}
-
-void ball_Draw(Ball ball)
-{
-    DrawCircle(ball.position.x, ball.position.y, ball.size, ball.color);
-}
-
-void ball_UpdatePosition(Ball *ball)
-{
-    ball->position.x += ball->velocity.x * ball->speed;
-    ball->position.y += ball->velocity.y * ball->speed;
-}
-
-void ball_CheckCollision(Ball *ball, Player *player, Enemy *enemy)
-{
-
-    if (ball->position.x <= 0 + ball->size)
-    {
-        ball->position = (Vector2){SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
-        ball->velocity.x = -(ball->velocity.x);
-        if (player->point != 0) player->point -= 1;
-        enemy->collision = true;
-    }
-    if (ball->position.x >= SCREEN_WIDTH - ball->size)
-    {
-        ball->position = (Vector2){SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
-        ball->velocity.x = -(ball->velocity.x);
-        if (enemy->point != 0) enemy->point -= 1;
-        player->collision = true;
+void enemy_UpdatePosition(Enemy *enemy, Ball ball) {
+  if (ball.position.x > SCREEN_WIDTH / 2) {
+    if (ball.position.y - ball.size < enemy->position) {
+      enemy->position -= enemy->speed;
+    } else if (ball.position.y + ball.size > (enemy->position) + (enemy->texture.height)) {
+      enemy->position += enemy->speed;
     }
 
-    if (ball->position.y <= 0 + ball->size) ball->velocity.y = -(ball->velocity.y);
-    if (ball->position.y >= SCREEN_HEIGHT - ball->size) ball->velocity.y = -(ball->velocity.y);
-
-    if (CheckCollisionCircleRec(ball->position, ball->size, player->texture) && player->collision)
-    {
-        ball->speed += 0.2f;
-        ball->speed += 0.2f;
-        ball->velocity.x = -(ball->velocity.x);
-        player->collision = false;
-        enemy->collision = true;
-    }
-    if (CheckCollisionCircleRec(ball->position, ball->size, enemy->texture) && enemy->collision)
-    {
-        ball->speed += 0.2f;
-        ball->speed += 0.2f;
-        ball->velocity.x = -(ball->velocity.x);
-        enemy->collision = false;
-        player->collision = true;
-    }
+    enemy->position = Clamp(enemy->position, 10, SCREEN_HEIGHT - 10 - (enemy->texture.height));
+    enemy->texture.y = enemy->position;
+  }
 }
 
-int main(void)
-{
-    Player player = {0};
-    Enemy enemy = {0};
-    Ball ball = {0};
+void ball_Draw(Ball ball) {
+  DrawCircle(ball.position.x, ball.position.y, ball.size, ball.color);
+}
 
-    player.color = BLACK;
-    player.texture = (Rectangle){10, 10, 40, 150};
-    player.speed = 5;
-    player.position = 0;
-    player.point = 3;
-    player.collision = true;
+void ball_UpdatePosition(Ball *ball) {
+  ball->position.x += ball->velocity.x * ball->speed;
+  ball->position.y += ball->velocity.y * ball->speed;
+}
 
-    enemy.color = BLACK;
-    enemy.texture = (Rectangle){SCREEN_WIDTH - 40 - 10, 10, 40, 150};
-    enemy.speed = 5;
-    enemy.position = 0;
-    enemy.point = 3;
-    enemy.collision = true;
+void ball_CheckCollision(Ball *ball, Player *player, Enemy *enemy) {
 
-    ball.color = BLACK;
-    ball.speed = 5;
-    ball.size = 25;
-    ball.position = (Vector2){SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
-    ball.velocity = (Vector2){1, 1};
+  if (ball->position.x <= 0 + ball->size) {
+    ball->position = (Vector2){SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
+    ball->velocity.x = -(ball->velocity.x);
+    if (player->point != 0)
+      player->point -= 1;
+    enemy->collision = true;
+  }
+  if (ball->position.x >= SCREEN_WIDTH - ball->size) {
+    ball->position = (Vector2){SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
+    ball->velocity.x = -(ball->velocity.x);
+    if (enemy->point != 0)
+      enemy->point -= 1;
+    player->collision = true;
+  }
 
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "pong");
-    SetTargetFPS(60);
-    SetExitKey(0);
+  if (ball->position.y <= 0 + ball->size)
+    ball->velocity.y = -(ball->velocity.y);
+  if (ball->position.y >= SCREEN_HEIGHT - ball->size)
+    ball->velocity.y = -(ball->velocity.y);
 
-    while (!WindowShouldClose())
-    {
-        player_UpdatePosition(&player);
-        enemy_UpdatePosition(&enemy, ball);
-        ball_UpdatePosition(&ball);
-        ball_CheckCollision(&ball, &player, &enemy);
+  if (CheckCollisionCircleRec(ball->position, ball->size, player->texture) &&
+      player->collision) {
+    ball->speed += 0.2f;
+    ball->speed += 0.2f;
+    ball->velocity.x = -(ball->velocity.x);
+    player->collision = false;
+    enemy->collision = true;
+  }
+  if (CheckCollisionCircleRec(ball->position, ball->size, enemy->texture) &&
+      enemy->collision) {
+    ball->speed += 0.2f;
+    ball->speed += 0.2f;
+    ball->velocity.x = -(ball->velocity.x);
+    enemy->collision = false;
+    player->collision = true;
+  }
+}
 
-        if (player.point == 0)
-        {
-            ball.color = RED;
-            ball.velocity = (Vector2){0, 0};
-        }
-        if (enemy.point == 0)
-        {
-            ball.color = RED;
-            ball.velocity = (Vector2){0, 0};
-        }
+int main(void) {
+  Player player = {0};
+  Enemy enemy = {0};
+  Ball ball = {0};
 
-        BeginDrawing();
-            ClearBackground(WHITE);
+  player.color = BLACK;
+  player.texture = (Rectangle){10, 10, 40, 150};
+  player.speed = 5;
+  player.position = 0;
+  player.point = 3;
+  player.collision = true;
 
-            DrawText(TextFormat("%d", player.point), 350, 5, 60, RED);
-            DrawText(TextFormat("%d", enemy.point), SCREEN_WIDTH - 350 - 60, 5, 60, RED);
+  enemy.color = BLACK;
+  enemy.texture = (Rectangle){SCREEN_WIDTH - 40 - 10, 10, 40, 150};
+  enemy.speed = 5;
+  enemy.position = 0;
+  enemy.point = 3;
+  enemy.collision = true;
 
-            player_Draw(player);
-            enemy_Draw(enemy);
-            ball_Draw(ball);
+  ball.color = BLACK;
+  ball.speed = 5;
+  ball.size = 25;
+  ball.position = (Vector2){SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
+  ball.velocity = (Vector2){1, 1};
 
-            if (player.point == 0)
-            {
-                DrawText("You lost", SCREEN_WIDTH / 2 - 120, SCREEN_HEIGHT / 2 - 30, 60, BLACK);
-            }
-            if (enemy.point == 0)
-            {
-                DrawText("You win", SCREEN_WIDTH / 2 - 120, SCREEN_HEIGHT / 2 - 30, 60, BLACK);
-            }
-        EndDrawing();
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "pong");
+  SetTargetFPS(60);
+  SetExitKey(0);
+
+  while (!WindowShouldClose()) {
+    player_UpdatePosition(&player);
+    enemy_UpdatePosition(&enemy, ball);
+    ball_UpdatePosition(&ball);
+    ball_CheckCollision(&ball, &player, &enemy);
+
+    if (player.point == 0) {
+      ball.color = RED;
+      ball.velocity = (Vector2){0, 0};
+    }
+    if (enemy.point == 0) {
+      ball.color = RED;
+      ball.velocity = (Vector2){0, 0};
     }
 
-    CloseWindow();
-    return 0;
+    BeginDrawing();
+    ClearBackground(WHITE);
+
+    DrawText(TextFormat("%d", player.point), 350, 5, 60, RED);
+    DrawText(TextFormat("%d", enemy.point), SCREEN_WIDTH - 350 - 60, 5, 60, RED);
+
+    player_Draw(player);
+    enemy_Draw(enemy);
+    ball_Draw(ball);
+
+    if (player.point == 0) {
+      DrawText("You lost", SCREEN_WIDTH / 2 - 120, SCREEN_HEIGHT / 2 - 30, 60, BLACK);
+    }
+    if (enemy.point == 0) {
+      DrawText("You win", SCREEN_WIDTH / 2 - 120, SCREEN_HEIGHT / 2 - 30, 60, BLACK);
+    }
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
 }
--- src/pong.h
+++ src/pong.h
@@ -1,48 +1,42 @@
-/* 
-@author: Gael Lopes Da Silva
-@project: Pong
-@github: https://github.com/Gael-Lopes-Da-Silva/Pong
-@gitlab: https://gitlab.com/Gael-Lopes-Da-Silva/Pong
-*/
+// @author: Gael Lopes Da Silva
+// @project: Pong
+// @github: https://github.com/Gael-Lopes-Da-Silva/Pong
 
 #pragma once
 
 #ifndef RAYLIB_LOADED
-    #include "raylib/raylib.h"
-    #include "raylib/raymath.h"
-    #include "raylib/reasings.h"
+#include "../include/raylib.h"
+#include "../include/raymath.h"
+#include "../include/reasings.h"
 #endif
 
 #define SCREEN_WIDTH 1360
 #define SCREEN_HEIGHT 760
 
-typedef struct Player
-{
-    int position;
-    int point;
-    float speed;
-    bool collision;
-    Rectangle texture;
-    Color color;
+typedef struct Player {
+  int position;
+  int point;
+  float speed;
+  bool collision;
+  Rectangle texture;
+  Color color;
 } Player;
 
-typedef struct Enemy
-{
-    int position;
-    int point;
-    float speed;
-    bool collision;
-    Rectangle texture;
-    Color color;
+typedef struct Enemy {
+  int position;
+  int point;
+  float speed;
+  bool collision;
+  Rectangle texture;
+  Color color;
 } Enemy;
 
-typedef struct Ball
-{
-    float size;
-    float speed;
-    Vector2 position;
-    Vector2 velocity;
-    Color color;
+typedef struct Ball {
+  float size;
+  float speed;
+  Vector2 position;
+  Vector2 velocity;
+  Color color;
 } Ball;
 
 void player_Draw(Player player);
